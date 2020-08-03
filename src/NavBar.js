74212b8b03388a677f5709ea237b10b8d91fc4ec
Tom <thomas.spencer@codurance.com>
2020-08-03 16:04:29 +0100

    Add config

---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginBottom: '10px'
+  }
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+          <Typography variant="h6">
+            Mars Rover - click the square to move the rover!
+          </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;

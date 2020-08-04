6ea54215c3fa85d5fdd8e46ccbaee4ac18ba28ca
Tom <thomas.spencer@codurance.com>
2020-08-04 14:43:01 +0100

    Reduce margin bottom

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
-    marginBottom: '20px'
+    marginBottom: '15px'
   },
   italic: {
     fontStyle: 'italic'
